cd52e1bf8ed05cf7f81d5fe077ab491365c49b0c
Cory Kim <github@cory.kim>
2018-12-05 18:40:37 -0600

    feat(error signal): Return an error signal if the service returns error

--- test/test.js
+++ test/test.js
@@ -1,31 +1,64 @@
 const assert = require('assert');
-const t = require('../index');
+const fs = require('fs');
 
-//const auth = require('./auth.json');
-const auth = null;
+const {
+  StripeApplet
+} = require('../index');
+
+const auth = fs.existsSync('./test/auth.json') ? require('./auth.json') : null;
+console.log(`Auth: ${JSON.stringify(auth)}`);
 
 describe('StripeApplet', function () {
-  let app = new t.StripeApplet();
-
   it('#constructor()', function () {
+    let app = new StripeApplet();
     assert.ok(app);
   });
 
-  it('#run()', function () {
-    if (auth) {
-      app.processConfig({
-        testMode: true,
-        authorization: auth,
-      });
-      app.run().then((signal) => {
-        console.log("Signal was: " + JSON.stringify(signal));
-        assert.ok(null == signal || signal);
-      }).catch(error => {
-        assert.fail(error);
-      });
+  describe('#run()', function () {
+    it('runs normally', async function () {
+      if (auth) {
+        return buildApp().then(async (app) => {
+          return app.run().then(async (signal) => {
+            assert.ok(null == signal || signal);
+          }).catch(error => {
+            assert.fail(error);
+          });
+        });
+      } else {
+        console.log("Skipping run test because auth.json does not exist.");
+      }
+    });
 
-    } else {
-      console.log("Skipping run test because auth.json does not exist.");
-    }
+    it('signals error when API key is bad', async function () {
+      if (auth) {
+        return buildApp({
+          authorization: {
+            apiKey: 'bad',
+          }
+        }).then(async (app) => {
+          return app.run().then(async (signal) => {
+            console.log("Signal was: " + JSON.stringify(signal));
+            assert.ok(null == signal || signal);
+            assert.equal('ERROR', signal.action);
+          }).catch(error => {
+            assert.fail(error);
+          });
+        });
+      } else {
+        console.log("Skipping run test because auth.json does not exist.");
+      }
+    })
+
   });
 })
+
+const defaultConfig = Object.freeze({
+  authorization: auth,
+});
+
+async function buildApp(config) {
+  let app = new StripeApplet();
+  return app.processConfig(config || defaultConfig).then(() => {
+    return app;
+  })
+}
